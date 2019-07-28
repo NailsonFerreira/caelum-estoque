76bf05e25447ea41bf91f151b9804c81061221dc
Nailson <nailson_ferreira@hotmail.com>
2019-07-28 16:00:25 -0300

    primeiro commit

---
+++ CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,58 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Index()
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            IList<Produto> produtos = dao.Lista();
+            ViewBag.Produtos = produtos;
+
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            ViewBag.Produto = new Produto();
+            CategoriasDAO dao = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = dao.Lista();
+            ViewBag.Categorias = categorias;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adiciona(Produto produto)
+        {
+
+            if (produto.Nome.Length < 5 || produto.Nome == null)
+            {
+                ModelState.AddModelError("tamanhoMinimo", "Tamanho minimo 5 caracteres");
+
+                //return View("Form");
+            }
+            if (ModelState.IsValid)
+            {
+            ProdutosDAO dao = new ProdutosDAO();
+            dao.Adiciona(produto);
+            }
+            else
+            {
+                ViewBag.Produto = produto;
+                CategoriasDAO dao = new CategoriasDAO();
+                ViewBag.Categorias = dao.Lista();
+
+                return View("Form");
+            }
+            return RedirectToAction("Index", "Produto");
+        }
+    }
+}
